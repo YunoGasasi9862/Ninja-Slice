099c5ffd678a62e3e7f6e0add3c9a1047e56950c
Muhammad Bilal <m.bilal9862@gmail.com>
2022-05-27 12:22:27 +0300

    melon

---
+++ Assets/Fruit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedwatermelon;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Blade")
+        {
+            Instantiate(slicedwatermelon);
+            Destroy(gameObject);
+          
+
+        }
+    }
+}
