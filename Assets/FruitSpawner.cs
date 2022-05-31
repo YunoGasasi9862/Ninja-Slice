5c7f2c3277c6d79c7a428cad28e38b6c1bd63586
Muhammad Bilal <m.bilal9862@gmail.com>
2022-05-31 12:38:43 +0300

    s

---
+++ Assets/FruitSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public GameObject fruit;
+    public Transform[] spawnPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
