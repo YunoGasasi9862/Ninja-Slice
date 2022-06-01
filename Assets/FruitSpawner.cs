2fdd3cf27f699c36649e9140a4113ca93435652b
Muhammad Bilal <m.bilal9862@gmail.com>
2022-06-01 13:13:37 +0300

    ww

--- Assets/FruitSpawner.cs
+++ Assets/FruitSpawner.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour
 {
-    public GameObject fruit;
+    public GameObject fruitprefab;
     public Transform[] spawnPoints;
+
+    public float minDelay = .1f;
+    public float maxDelay = 1f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnFruits()
     {
-        
+
+        while(true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);  //it waits or delays the next execution of the line :))
+
+
+            int spawnIndex = Random.Range(0, spawnPoints.Length); //between 0 to the max sizeo f the array
+
+            Instantiate(fruitprefab);
+
+
+
+        }
+
     }
 }
